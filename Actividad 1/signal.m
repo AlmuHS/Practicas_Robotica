4f6bee71ed5142950493904070c5332d10f600de
AlmuHS <liberamenso10000@gmail.com>
2018-10-19 11:35:05 +0200

    modificado signal para puesta en cero fuera de rango

--- Actividad 1/signal.m
+++ Actividad 1/signal.m
@@ -1,3 +1,7 @@
-function out = signal(amplitud, tiempo)
-    out = amplitud*sin(tiempo);
+function out = signal(amplitud, tiempo, periodo, desfase)
+        if tiempo < desfase | tiempo > periodo+desfase
+            out = 0:0;
+        else
+            out = amplitud*sin((2*pi/periodo)*(tiempo-desfase));
+        end 
 end
