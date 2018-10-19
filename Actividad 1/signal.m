e95006f778e83335b86ebddb516d5e92c8e1b09d
AlmuHS <liberamenso10000@gmail.com>
2018-10-19 11:38:47 +0200

    mejorada sintaxis

--- Actividad 1/signal.m
+++ Actividad 1/signal.m
@@ -1,5 +1,5 @@
 function out = signal(amplitud, tiempo, periodo, desfase)
-        if tiempo < desfase | tiempo > periodo+desfase
+        if (tiempo < desfase) | (tiempo >= periodo+desfase)
             out = 0:0;
         else
             out = amplitud*sin((2*pi/periodo)*(tiempo-desfase));
