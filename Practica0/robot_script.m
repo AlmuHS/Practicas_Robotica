55882ce0ba0d4c9f3e79d31a799acc93d73f46bb
AlmuHS <liberamenso10000@gmail.com>
2018-11-08 12:52:17 +0100

    anadida practica 0

---
+++ Practica0/robot_script.m
@@ -0,0 +1,38 @@
+clear tstart
+clear anguloCabeza
+
+tstart = tic;
+
+%Definir motores y sensores
+motorCabeza = NXTMotor('C');
+motorRuedaIzq = NXTMotor('B');
+motorRuedaDer = NXTMotor('A');
+
+i = 0;
+tiempo(i) = 0;
+
+A = 0;
+B = 1;
+C = 2;
+
+tiempo(i) = toc(tstart);
+
+while tiempo(i) < 2
+    i = i+1;
+    tiempo(i) = toc(tstart);
+    
+    %Mandar orden motor cabeza -> POWER = 50
+    motorCabeza.Power = 50;
+    motorCabeza.SendToNXT();
+    
+    %Leer encoder cabeza -> Angulo_Cabeza[i]
+    anguloCabeza(i) = NXT_GetOutputState(B); 
+    
+    pinta_robot(0, 0, 0, anguloCabeza(i));
+    
+end
+
+%Parar motor cabeza
+motorCabeza.Stop('off'); 
+
+plot(tiempo, AnguloCabeza);
