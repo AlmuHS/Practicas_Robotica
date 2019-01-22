e0cbf59f5d2c6360a82357e9b14bc8a39d0a6408
AlmuHS <liberamenso10000@gmail.com>
2019-01-22 18:12:14 +0100

    borrada basura

--- Practica1/Practica2/signal.m
+++
@@ -1,8 +0,0 @@
-
-function out = signal(amplitud, tiempo, periodo, desfase)
-        if (tiempo < desfase) | (tiempo >= periodo+desfase)
-            out = 0:0;
-        else
-            out = amplitud*sin((2*pi/periodo)*(tiempo-desfase));
-        end 
-end
